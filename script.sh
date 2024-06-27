6882ed0ff108c04af034635cb28c2cd59a3499fc
Param Birje <parambirje@gmail.com>
2024-06-27 10:35:10 +0530

    Added commands for installing python, augeas-libs, certbot

--- script.sh
+++ script.sh
@@ -13,3 +13,21 @@ echo "Nginx is running."
 
 # Enabling nginx to start on boot
 systemctl enable nginx
+
+# Installing python3 and augeas-libs
+# for later installing certbot with pip
+dnf install python3 augeas-libs
+
+# Removing existing certbot installation
+dnf remove certbot
+
+# Installing certbot with a virtual environment
+python3 -m venv /opt/certbot/
+/opt/certbot/bin/pip install --upgrade pip
+/opt/certbot/bin/pip install certbot certbot-nginx
+
+# Creating a symbolic link to the certbot binary
+ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+
+# Running certbot to get the certificate
+certbot --nginx
