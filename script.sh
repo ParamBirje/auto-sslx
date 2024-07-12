49c1e38f44f8e7990bcc0426cfacec250fe08f49
Param Birje <parambirje@gmail.com>
2024-07-12 18:23:18 +0530

    Added \ to escape the $ sign in server block headers

--- script.sh
+++ script.sh
@@ -73,10 +73,10 @@ server_block="server {
                 proxy_pass http://127.0.0.1:$service_port;
 
                 # Default HTTP headers
-                proxy_set_header Host $host;
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                proxy_set_header X-Forwarded-Proto $scheme;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto \$scheme;
 
                 # Add custom headers here to forward to your service
                 # ---
